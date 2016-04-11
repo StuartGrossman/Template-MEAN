e8501fc46166ad1b2c366326dad07c8ad41e092f
Stuart Grossman <grossman.stuart1@gmail.com>
2016-04-11 15:40:12 -0700

    check 2

--- public/javascript/controllers/MapController.js
+++
@@ -1,6 +0,0 @@
-(function(){
-    'use strict'
-    angular.module('app').controller('MapCtrl', function ($scope) {
-
-  
-})();
---
+++ public/javascript/controllers/MovieController.js
@@ -0,0 +1,13 @@
+(function(){
+	'use strict'
+	angular.module('app').controller('MovieController', MovieController);
+	MovieController.$inject = ['$state'];
+
+	function MovieController($state){
+		var vm = this;
+		
+
+
+	}
+
+})()
